50fac4fa1145fd708bc4e3635ef3e461e4ad8d5f
Randamanz <randa.jtd@gmail.com>
2017-09-15 22:03:35 +0100

    Update Constants to be universal

--- Constants.cs
+++ Constants.cs
@@ -2,7 +2,7 @@
 {
     internal class Constants
     {
-        public const string DBCONNSTRING = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\joseph.dowling\Documents\AddressBook.accdb;Persist Security Info=False";
+        public const string DBCONNSTRING = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=AddressBook.accdb;Persist Security Info=False";
         
     }
 }
